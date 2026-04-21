d3d7d128f9b577c869090886fcda91a0163a678d
Justin Prince <justin.prince@chainguard.dev>
2026-04-21 07:45:18 -0400

    Removing old folders.

--- single-image-copy-ecr/iac/main.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_providers {
-    aws        = { source = "hashicorp/aws" }
-    chainguard = { source = "chainguard-dev/chainguard" }
-    ko         = { source = "ko-build/ko" }
-  }
-}
